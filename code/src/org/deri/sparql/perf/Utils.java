1d9345e6d6785632407e0dd248023fa958412bb8
jueumb <jueumb@gmail.com>
2013-07-18 09:31:58 +0100

    deleted code

--- code/src/org/deri/sparql/perf/Utils.java
+++
@@ -1,8 +0,0 @@
-package org.deri.sparql.perf;
-
-public class Utils {
-	public static String removeNewlines(String in){
-		if(in == null) return null;
-		return in.replaceAll("\r", "  ").replaceAll("\n", "  ");
-	}
-}
--- code/src/sparqldiscovery/Main.java
+++
@@ -1,9 +0,0 @@
-package sparqldiscovery;
-
-public class Main {
-
-	
-	
-	
-	
-}
