08f2a89eccb6949e0bf2f8178f50fc4512883856
Pierre-Yves Vandenbussche <py.vandenbussche@gmail.com>
2013-10-29 11:12:16 +0000

    Add iswc2013 resources

--- node/app.js
+++ node/app.js
@@ -254,84 +254,12 @@ app.get('/interoperability', function(req, res){
 		});
 });
 
+app.get('/iswc2013', function(req, res){
+		mongoDBProvider.autocomplete(req.param('q'), function(error,docs){
+			res.render('content/iswc2013.jade',{});
+		});
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
- 
- 
- 
-/*
-var express = require('express');
-var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
-
-var app = module.exports = express();
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-var articleProvider = new ArticleProvider('localhost', 27017);
-
-app.get('/', function(req, res){
-    articleProvider.findAll( function(error,docs){
-        res.render('index.jade', {
-            title: 'Blog',
-            articles:docs
-            }
-        );
-    })
-});
-
-app.get('/blog/new', function(req, res) {
-    res.render('blog_new.jade', {
-        title: 'New Post'
-    });
-});
-
-app.post('/blog/new', function(req, res){
-    articleProvider.save({
-        title: req.param('title'),
-        body: req.param('body')
-    }, function( error, docs) {
-        res.redirect('/')
-    });
-});
-
-app.post('/blog/addComment', function(req, res) {
-    articleProvider.addCommentToArticle(req.param('_id'), {
-        person: req.param('person'),
-        comment: req.param('comment'),
-        created_at: new Date()
-       } , function( error, docs) {
-           res.redirect('/blog/' + req.param('_id'))
-       });
-});
-
-app.get('/blog/:id', function(req, res) {
-    articleProvider.findById(req.params.id, function(error, article) {
-        res.render('blog_show-final.jade',
-        { locals: {
-            title: article.title,
-            article:article
-        }
-        });
-    });
-});
-
-app.listen(3000);
-//console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-*/
